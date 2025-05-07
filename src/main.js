1cd4e3a14d43624d1bea878b7adcdf557348962c
bopensh <76118888+bopensh@users.noreply.github.com>
2025-05-07 15:13:44 +0100

    Update: Started building out index.html
    Started building out index.html

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Mobile menu toggle
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('menu-toggle');
+  const menu = document.getElementById('mobile-menu');
+  toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+});
